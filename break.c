6c3b1f1ac123170c3bca6187d559b9c63f352fa4
vaibhav2777 <vaibhav.agarwal2701@gmail.com>
2020-06-24 20:10:18 +0530

    Modified break.c

--- break.c
+++ break.c
@@ -2,18 +2,18 @@
 int main()
 {
     char name[20];
-    printf("Enter your name:", name);
+    printf("Enter your name:");
     scanf("%s", &name);
      int i,age;
      for ( i = 0; i < 10; i++)
      {
         printf("%d\n", i);
         printf("%s Enter your age:\n",name);
-        scanf("%s", &age);
+        scanf("%d", &age);
         if (age==10)
         {
-            printf("Please enter your actual age:", age);
-            scanf("%d", &age);
+            //printf("Please enter your actual age:", age);
+            //scanf("%d", &age);
             break;
         }
         
