b2e8d6d27c6de65f070182441ed4064c1e6e8a1f
vaibhav2777 <vaibhav.agarwal2701@gmail.com>
2020-07-07 20:07:03 +0530

    New Files on C

---
+++ prog7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int chars, i = 0;
+    char *ptr;
+    while (i < 3)
+    {
+        printf("Employee %d: Enter the number of characters in your Employee ID\n ", i + 1);
+        scanf("%d", &chars);
+        ptr = (char *)malloc((chars + 1) * sizeof(char));
+        printf("Enter your Employee ID\n");
+        scanf("%s", ptr);
+        printf("Your Employee ID is %s\n", ptr);
+        free(ptr);
+        i = i + 1;
+    }
+    return 0;
+}
